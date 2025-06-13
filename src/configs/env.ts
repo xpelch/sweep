4fa11795ceb1b6751b2826679cc452272da6a188
Xavier Pelchat <77986237+xpelch@users.noreply.github.com>
2025-06-13 16:11:36 -0400

    Initial push

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,3 +1,1 @@
 export const PUBLIC_RPC_URL = 'https://base-rpc.publicnode.com';
-export const RPC_URL = 'https://base-mainnet.g.alchemy.com/v2/Ya2m99qp1xfoCOIE0VBpN';
-export const ZERO_X_API_KEY = 'fd47e4f0-3bf9-47fc-9060-df7919ba9974';
